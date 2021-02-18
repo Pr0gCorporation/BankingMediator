073500552b145c1e93dd55e066054c254b5254da
LAPTOP-100056\ruslan.rudenko <>
2021-02-18 16:08:25 +0300

    sftp download

--- SftpService/Internship.SftpService.Service/SFTPAccess/DownloadFileFromServer.cs
+++ SftpService/Internship.SftpService.Service/SFTPAccess/DownloadFileFromServer.cs
@@ -31,6 +31,7 @@ namespace Internship.SftpService.Service.SFTPAccess
 
             sftp.Connect();
             logger.LogInformation($"Connect to sftp: {sftp.IsConnected} .\n\n");
+            
             using (Stream fileStream = File.Create(fileName))
             {
                 sftp.DownloadFile(fullpaths, fileStream);
@@ -40,6 +41,7 @@ namespace Internship.SftpService.Service.SFTPAccess
                 sftp.DeleteFile(fullpaths);
                 logger.LogInformation($"After file deleted: \n{sftp.ListDirectory(path)} .\n\n");
             }
+            
             sftp.Disconnect();
             logger.LogInformation($"Connect from sftp: {sftp.IsConnected} .\n\n");
 
