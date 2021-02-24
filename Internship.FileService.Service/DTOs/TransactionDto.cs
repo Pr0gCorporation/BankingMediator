e357b06d4c4bc9db39cfc1aafd42d2790acc8a52
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-24 12:24:23 +0300

    add transaction dto

---
+++ Internship.FileService.Service/DTOs/TransactionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Internship.FileService.Service.DTOs
+{
+    public class TransactionDto
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string FileName { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
