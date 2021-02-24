689ca3392f3cf4110e4f91a21347cbf9a728c0cd
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-24 13:10:32 +0300

    hardcode file consumer

--- Internship.FileService.Service/Consumers/FileConsumer.cs
+++ Internship.FileService.Service/Consumers/FileConsumer.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Internship.FileService.Service.DTOs;
 using Internship.SftpService.Service.DTOs;
 using MassTransit;
 using Microsoft.Extensions.Logging;
@@ -20,7 +25,20 @@ namespace Internship.FileService.Service.Consumers
         {
             _logger.LogWarning($"Look! I've got a new file: {context.Message.Name}, " +
                                $"\ndate = {context.Message.Date}, " +
-                               $"\nbytes[] = {context.Message.File}");
+                               $"\nbytes[] = {context.Message.File}\n");
+
+            await using var memoryStream = new MemoryStream(context.Message.File);
+            using var streamReader = new StreamReader(memoryStream);
+            var xmlSerializer = new XmlSerializer(typeof(TransactionDto));
+            var transaction = (TransactionDto)xmlSerializer.Deserialize(streamReader);
+            if (transaction != null)
+            {
+                Console.WriteLine(transaction.Id.ToString());
+                Console.WriteLine(transaction.FileName);
+                Console.WriteLine(transaction.Date);
+                Console.WriteLine(transaction.From);
+                Console.WriteLine(transaction.To);
+            }
         }
     }
 }
