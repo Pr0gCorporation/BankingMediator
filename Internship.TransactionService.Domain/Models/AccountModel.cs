2f84adc21b1b7c17cfb7b1bbc63c2257771e944d
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-03 12:52:57 +0300

    Add transaction service domain models

--- Internship.TransactionService.Domain/Models/AccountModel.cs
+++ Internship.TransactionService.Domain/Models/AccountModel.cs
@@ -2,6 +2,10 @@
 {
     public class AccountModel
     {
-        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankId { get; set; } = "0000";
     }
 }
--- Internship.TransactionService.Domain/Models/TransactionModel.cs
+++ Internship.TransactionService.Domain/Models/TransactionModel.cs
@@ -1,7 +1,13 @@
-﻿namespace Internship.TransactionService.Domain.Models
+﻿using System;
+
+namespace Internship.TransactionService.Domain.Models
 {
     public class TransactionModel
     {
-        
+        public int DebtorId { get; set; }
+        public int CreditorId { get; set; }
+        public decimal Amount { get; set; } = 0;
+        public Guid TransactionId { get; set; }
+        public int Id { get; set; }
     }
 }
