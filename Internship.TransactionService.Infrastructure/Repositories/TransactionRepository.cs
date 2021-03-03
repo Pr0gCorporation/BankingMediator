e6bd14b532f3e6aa0232c5f32e7672d311e60230
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-03 14:09:33 +0300

    transaction repository implementation

--- Internship.TransactionService.Infrastructure/Repositories/TransactionRepository.cs
+++ Internship.TransactionService.Infrastructure/Repositories/TransactionRepository.cs
@@ -1,7 +1,38 @@
-﻿namespace Internship.TransactionService.Infrastructure.Repositories
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using Internship.TransactionService.Application.Repository.TransactionRepository;
+using Internship.TransactionService.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Internship.TransactionService.Infrastructure.Repositories
 {
-    public class TransactionRepository
+    public class TransactionRepository : ITransactionRepository
     {
-        
+        private readonly IConfiguration _configuration;
+
+        public TransactionRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IEnumerable<TransactionModel>> GetAll()
+        {
+            var sqlExpressionToGetAllTransactions = $"SELECT * FROM `transactionservice_db`.`transactions`";
+
+            await using var connection = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+            
+            var transactions = await connection.QueryAsync<TransactionModel>(sqlExpressionToGetAllTransactions, connection);
+            return transactions;
+        }
+
+        public async Task Add(TransactionModel entity)
+        {
+            // var sqlExpressionToInsertTransaction = "INSERT INTO `fileservice_db`.`files`"+
+            //                                        "(`date`) VALUES (@Date);";
+            //
+        }
     }
 }
