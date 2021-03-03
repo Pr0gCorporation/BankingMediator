f2401b8c19b2da57dc04c9b5d6d134d747eaec28
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-03 12:53:18 +0300

    Add transaction domain model

--- Internship.FileService.Domain/Models/Transaction.cs
+++ Internship.FileService.Domain/Models/Transaction.cs
@@ -8,7 +8,7 @@ namespace Internship.FileService.Domain.Models
     public class Transaction
     {
         public int TransactionId { get; set; }
-        public decimal Sum { get; set; }
+        public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public string Creditor { get; set; }
         public string Debtor { get; set; }
