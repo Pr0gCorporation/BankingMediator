7153065594e28fd90b4e25d84415388ea9620994
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-04 14:45:36 +0300

    Add publisher, mappers and brand new endpoint to post, revove hard coded data

--- Internship.TransactionService.API/Controllers/TransactionController.cs
+++ Internship.TransactionService.API/Controllers/TransactionController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Internship.FileService.Domain.Models;
 using Internship.TransactionService.API.DTOs.Account;
 using Internship.TransactionService.API.DTOs.Transaction;
 using Internship.TransactionService.Application.Repository.TransactionRepository;
+using Internship.TransactionService.Domain.Models;
+using MassTransit;
+using MassTransit.Initializers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Internship.TransactionService.API.Controllers
@@ -14,88 +20,43 @@ namespace Internship.TransactionService.API.Controllers
     public class TransactionController : Controller
     {
         private readonly ITransactionRepository _transactionRepository;
-        
+        private readonly IMapper _mapper;
+        private readonly IBus _publisher;
+
         public TransactionController(
-            ITransactionRepository transactionTransactionRepository)
+            ITransactionRepository transactionTransactionRepository, IMapper mapper, IBus publisher)
         {
-             _transactionRepository = transactionTransactionRepository;
+            _transactionRepository = transactionTransactionRepository;
+            _mapper = mapper;
+            _publisher = publisher;
         }
         
         // GET: api/Transactions
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TransactionReadDto>>> Get()
         {
-            return Ok(new List<TransactionReadDto>() // Hard code for now
-            {
-                new TransactionReadDto()
-                {
-                    Amount = 290,
-                    Debtor = new AccountReadDto()
-                    {
-                        AccountNumber = "432523",
-                        BankId = "4422",
-                        FirstName = "John",
-                        LastName = "Smith"
-                    },
-                    Creditor = new AccountReadDto()
-                    {
-                        AccountNumber = "432444",
-                        BankId = "3324",
-                        FirstName = "Will",
-                        LastName = "Brendon"
-                    },
-                    TransactionId = new Guid()
-                },
-                new TransactionReadDto()
-                {
-                    Amount = 5344,
-                    Debtor = new AccountReadDto()
-                    {
-                        AccountNumber = "432655",
-                        BankId = "3342",
-                        FirstName = "Deni",
-                        LastName = "Washington"
-                    },
-                    Creditor = new AccountReadDto()
-                    {
-                        AccountNumber = "782444",
-                        BankId = "3334",
-                        FirstName = "Will",
-                        LastName = "Cargo"
-                    },
-                    TransactionId = new Guid()
-                },
-                new TransactionReadDto()
-                {
-                    Amount = 13999,
-                    Debtor = new AccountReadDto()
-                    {
-                        AccountNumber = "434544",
-                        BankId = "8777",
-                        FirstName = "John",
-                        LastName = "Mankind"
-                    },
-                    Creditor = new AccountReadDto()
-                    {
-                        AccountNumber = "343423",
-                        BankId = "1265",
-                        FirstName = "Brad",
-                        LastName = "Smith"
-                    },
-                    TransactionId = new Guid()
-                }
-            });
+            var transactions = await _transactionRepository.GetAll();
+            return Ok(_mapper.Map<IEnumerable<TransactionReadDto>>(transactions));
         }
 
         // POST: api/Transactions
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] TransactionCreateDto transaction)
         {
-            //
-            // TODO: 
-            // 1) Insert to DB
-            // 2) Publish to the File Service Endpoint
-            //
+            // Instance to insert
+            var transactionModel = _mapper.Map<TransactionModel>(transaction);
+
+            // Insert to DB
+            await _transactionRepository.Add(transactionModel);
+            
+            // Instances to publish
+            var transactionsFullModel = (await _transactionRepository.GetByTransactionId(transaction.TransactionId));
+            
+            var transactionRead = _mapper.Map<TransactionReadDto>(transactionsFullModel);
+            var transactionFile = _mapper.Map<Transaction>(transactionRead);
+            
+            // Publish to file endpoint
+            await _publisher.Publish(transactionFile);
             
             return Ok();
         }
--- Internship.TransactionService.API/DTOs/Transaction/TransactionCreateDto.cs
+++ Internship.TransactionService.API/DTOs/Transaction/TransactionCreateDto.cs
@@ -6,8 +6,8 @@ namespace Internship.TransactionService.API.DTOs.Transaction
     public class TransactionCreateDto
     {
         public int Id { get; set; }
-        public AccountCreateDto Debtor { get; set; }
-        public AccountCreateDto Creditor { get; set; }
+        public int DebtorId { get; set; }
+        public int CreditorId { get; set; }
         public decimal Amount { get; set; } = 0;
         public Guid TransactionId { get; set; }
     }
