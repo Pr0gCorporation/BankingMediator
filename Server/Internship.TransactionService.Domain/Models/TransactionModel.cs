22b6df6bbe23bd4166c4151fea41d2fe28c77352
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-05 11:37:52 +0300

    Expand transaction domain model from accouts models to plain properties in the transaction model

--- Server/Internship.TransactionService.Domain/Models/TransactionModel.cs
+++ Server/Internship.TransactionService.Domain/Models/TransactionModel.cs
@@ -5,8 +5,14 @@ namespace Internship.TransactionService.Domain.Models
     public class TransactionModel
     {
         public int Id { get; set; }
-        public AccountModel Debtor { get; set; }
-        public AccountModel Creditor { get; set; }
+        public string DebtorFirstName { get; set; }
+        public string DebtorLastName { get; set; }
+        public string DebtorAccountNumber { get; set; }
+        public string DebtorBankId { get; set; }
+        public string CreditorFirstName { get; set; }
+        public string CreditorLastName { get; set; }
+        public string CreditorAccountNumber { get; set; }
+        public string CreditorBankId { get; set; }
         public decimal Amount { get; set; }
         public Guid TransactionId { get; set; }
     }
