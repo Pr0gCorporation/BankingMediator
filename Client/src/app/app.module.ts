a8715c96ff72488c5d2cfbd2d16bc98dbd74c375
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-05 22:42:42 +0300

    app module add things to work

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { TransactionsListComponent } from './transactions-list/transactions-list.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionsListComponent,
+    AddTransactionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
