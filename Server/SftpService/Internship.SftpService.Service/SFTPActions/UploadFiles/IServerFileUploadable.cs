4d96a45396bebbbeee5f02ac441cc72059224930
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-05 11:46:28 +0300

    remove file indexes to stop tracking ones

--- Server/SftpService/Internship.SftpService.Service/SFTPActions/UploadFiles/IServerFileUploadable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Internship.SftpService.Service.SFTPActions.UploadFiles
-{
-    public interface IServerFileUploadable
-    {
-        int Upload(string pathTo, string pathFrom, bool removeFileAfterDownloading); 
-        void Upload(string pathTo, byte[] file, string filename); 
-    }
-}
