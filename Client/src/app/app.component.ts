47e202d3c33c64b6ed58bd183f733d8fd14e0851
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-05 22:42:22 +0300

    add styles and title

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'BankingInternshipClient';
+  title: string = 'BankingInternshipClient';
+  appTitle: string = "Banking Internship Project!";
+  response: any;
 }
