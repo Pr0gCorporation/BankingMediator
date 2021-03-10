f0848144ed04d032daf3893a96520f0324c7e6af
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-10 18:57:44 +0300

    fix gitignore, add repo to file service

---
+++ Server/Internship.FileService.Domain/Interfaces/IFileRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Internship.FileService.Domain.Interfaces
+{
+    public interface IFileRepository
+    {
+        Task<int> Add(DateTime date, bool type, string filename, byte[] file);
+    }
+}
---
+++ Server/Internship.FileService.Domain/Models/Transaction/AccountToFile.cs
@@ -0,0 +1,10 @@
+﻿namespace Internship.FileService.Domain.Models.Transaction
+{
+    public class AccountToFile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankId { get; set; }
+    }
+}
