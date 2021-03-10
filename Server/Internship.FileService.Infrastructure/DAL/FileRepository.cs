20b865df02164e0c4b4333ab168764c035eed237
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-10 18:57:54 +0300

    data access layer for file service

---
+++ Server/Internship.FileService.Infrastructure/DAL/FileRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapper;
+using Internship.FileService.Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Internship.FileService.Infrastructure.DAL
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public FileRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<int> Add(DateTime date, bool type, string filename, byte[] file)
+        {
+            const string sqlExpressionToInsert = "INSERT INTO `fileservice_db`.`files`"+
+                                                 "(`date`, `incoming`, `filename`, `file`) VALUES (@Date, @Type, @FileName, @File);";
+
+            await using var connection = new MySqlConnection(_configuration.GetConnectionString("MYSQLConnection"));
+            connection.Open();
+            
+            var inserted = await connection.ExecuteAsync(sqlExpressionToInsert, new
+            {
+                Date = date,
+                Type = type,
+                FileName = filename,
+                File = file
+            });
+
+            return inserted;
+        }
+    }
+}
