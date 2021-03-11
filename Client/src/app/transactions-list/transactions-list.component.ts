18b31373c76c1610bbdaa28c56ea1bd92ea0e67f
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-11 18:41:04 +0300

    translate transaction list component

--- Client/src/app/transactions-list/transactions-list.component.ts
+++ Client/src/app/transactions-list/transactions-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TransactionsService } from '../shared/transactions.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-transactions-list',
@@ -10,7 +11,15 @@ export class TransactionsListComponent implements OnInit {
 
   public loading: boolean = true;
 
-  constructor(public transactionService: TransactionsService) { }
+  constructor(public transactionService: TransactionsService,
+    public translate: TranslateService)
+    {
+      translate.addLangs(['en', 'nl']);
+      translate.setDefaultLang('en');
+
+      const browserLang = translate.getBrowserLang();
+      translate.use(browserLang.match(/en|nl/) ? browserLang : 'en');
+    }
 
   ngOnInit(): void {
     this.transactionService.fetchTransactions().subscribe(() => {
