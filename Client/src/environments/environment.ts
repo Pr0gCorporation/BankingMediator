d660c35d7c5372e1be385142d35a6cab6587bb99
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-16 13:56:18 +0300

    add endpoint to cancel transaction

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:5001/api',
-  transactionUrlPart: '/Transaction'
+  baseUrl: 'https://localhost:44374',
+  transactionUrlPart: '/api/Transaction',
+  transactionCancelUrlPart: '/cancel',
 };
 
 /*
