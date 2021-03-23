9551c17f3a8da9b9efcd75f9508c60a105194e94
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-23 18:09:03 +0300

    Add account service domain models

---
+++ Server/Internship.AccountService.Domain/Models/CashbookRecordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Internship.AccountService.Domain.Models
+{
+    public class CashbookRecordModel
+    {
+        public int Id { get; set; }
+        public int CashbookId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string OriginReference { get; set; }
+    }
+}
