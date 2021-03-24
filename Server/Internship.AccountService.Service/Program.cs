19c6c42434a18e0b4ef4b55f8c43b0f677912693
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-24 13:34:55 +0300

    set up serilog and other services

--- Server/Internship.AccountService.Service/Program.cs
+++ Server/Internship.AccountService.Service/Program.cs
@@ -1,5 +1,12 @@
+using Internship.AccountService.Domain.Interfaces;
+using Internship.AccountService.Service.Consumers;
+using Internship.AccountService.Infrastructure.DAL;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace Internship.AccountService.Service
 {
@@ -7,14 +14,57 @@ namespace Internship.AccountService.Service
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The Application failed to start.");
+                throw;
+            }
+            finally
+            {
+                Log.Information("Application Stopping Down");
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
+                    var configuration = hostContext.Configuration;
+
+                    services.AddTransient<IAccountRepository, AccountRepository>();
+
+                    services.AddMassTransit(config =>
+                    {
+                        config.AddConsumer<UpdateAccountBalanceConsumer>();
+
+                        config.UsingRabbitMq((ctx, cfg) =>
+                        {
+                            cfg.Host(configuration.GetValue<string>("BusConfig:Host"));
+                             
+                            cfg.ReceiveEndpoint("update_balance", c =>
+                            {
+                                c.ConfigureConsumer<UpdateAccountBalanceConsumer>(ctx);
+                            });
+                        });
+                    });
+
+                    services.AddMassTransitHostedService();
                 });
     }
 }
