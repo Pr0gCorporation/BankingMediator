fded0c06972febd47dbdaf0771ff2e89b599ed91
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-04 19:24:42 +0300

    initialize tests for serializers

---
+++ Server/Internship.FileService.Service.Tests/SerializationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.FileService.Infrastructure.Tests
+{
+    public class SerializationTests
+    {
+        public SerializationTests()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /*
+                Test deserialization from string to TransactionFileModel or TransactionCSVFileModel
+         */
+    }
+}
