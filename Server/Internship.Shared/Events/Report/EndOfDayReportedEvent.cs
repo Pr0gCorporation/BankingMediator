64d02e0df5577ec912eb46ae00ffa77deed50c37
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-06 14:21:18 +0300

    Report-Generated event

---
+++ Server/Internship.Shared/Events/Report/EndOfDayReportedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.Shared.Events.Report
+{
+    public class EndOfDayReportedEvent
+    {
+        public DateTime Date { get; set; }
+        public List<Report> Reports { get; set; }
+    }
+}
---
+++ Server/Internship.Shared/Events/Report/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.Shared.Events.Report
+{
+    public class Report
+    {
+        public string IBAN { get; set; }
+        public decimal OpeningBalance { get; set; }
+        public decimal ClosingBalance { get; set; }
+        public List<BalanceMutation> BalanceMutations { get; set; }
+    }
+}
